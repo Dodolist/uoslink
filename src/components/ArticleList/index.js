6e415183a0c146be0a6f08f126da6f82130dab9d
Dodolist <dodolist3@gmail.com>
2023-11-15 02:46:05 +0900

    feat: ArticleList padding 범위 조절 및 style 수정

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import loadingIcon from '../../images/loading-icon.svg';
-import styled, { css, keyframes } from 'styled-components';
+import { ArticleListContainer, LoadingIcon, ArticleItemContainer, ArticleWrapper, ArticleInfoWrapper, ArticleRank, ArticleInfo, ArticleTitle, ListName } from './style';
 
 const SectionList = [
   {
@@ -110,7 +110,7 @@ const ArticleList = ({openViewer}) => {
 
     return (
       <ArticleItemContainer onClick={() => clickArticleItem(data.id, data.section, data.link)}>
-        <ArticleWrapper>
+        <ArticleWrapper alreadyRead={alreadyRead}>
           <ArticleRank>{rank}</ArticleRank>
           <ArticleInfoWrapper>
             <ArticleInfo>{sectionName}</ArticleInfo>
@@ -125,114 +125,3 @@ const ArticleList = ({openViewer}) => {
 export default React.memo(ArticleList, (prevProps, nextProps) => {
     return prevProps.selectedSection === nextProps.selectedSection;
 });
-const rotateAnimation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(1080deg);
-  } 
-`;
-
-const showListAnimation = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  } 
-`;
-
-const ArticleListContainer = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  height: 50%;
-  background-color: ${props => props.theme.foreground};
-  border-radius: 8px;
-  padding: 12px;
-  gap: 8px;
-
-  -ms-overflow-style: none; /* 인터넷 익스플로러 */
-  scrollbar-width: none; /* 파이어폭스 */
-  &::-webkit-scrollbar {
-    display: none; /* 크롬, 사파리, 오페라 브라우저 */
-  }
-`
-
-const LoadingIcon = styled.img`
-  animation: ${rotateAnimation} 4s cubic-bezier(.25,.51,.43,.7) infinite;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  translate: -50% -50%;
-  width: 48px;
-  height: 48px;
-`
-
-const ArticleItemContainer = styled.div`
-  position: relative;
-  animation: ${showListAnimation} 0.5s ease-in-out forwards;
-  opacity: 0;
-
-  display: flex;
-  flex-direction: column;
-
-
-  cursor: pointer;
-  text-decoration: none;
-
-  &:hover {
-    background-color: ${props => props.theme.foreground};
-    filter: ${props => props.showOption ? 'brightness(1)' : 'brightness(0.9)'};
-  }
-  &:active {
-    filter: ${props => props.showOption ? 'brightness(1)' : 'brightness(0.8)'};
-  }
-`
-
-const ArticleWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 8px;
-`;
-
-const ArticleInfoWrapper = styled.div`
-  width: calc(100% - 12px);
-  display: flex;
-  flex-direction: column;
-`
-
-const ArticleRank = styled.div`
-  color: ${props => props.theme.primary};
-  font-size: 10px;
-  font-weight: 500;
-`
-
-const ArticleInfo = styled.div`
-  color: ${props => props.theme.subText};
-  font-size: 10px;
-`
-
-const ArticleTitle = styled.div`
-  color: ${props => props.theme.contentText};
-  font-size: 12px;
-  font-weight: 500;
-
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`
-
-const ListName = styled.div`
-  animation: ${showListAnimation} 0.5s ease-in-out forwards;
-  color: ${props => props.theme.contentText};
-  font-size: 16px;
-  font-weight: bold;
-  margin-bottom: 4px;
-  opacity: 0;
-`
