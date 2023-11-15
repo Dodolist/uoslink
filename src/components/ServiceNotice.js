2e11afd468cbd1c5c097fdb7a776495a851e0b58
Dodolist <dodolist3@gmail.com>
2023-11-16 07:42:04 +0900

    버전 0.9.1

--- src/components/ServiceNotice.js
+++ src/components/ServiceNotice.js
@@ -64,7 +64,7 @@ const ServiceNotice = () => {
     try {
       url = 'https://www.iflab.run/api/check/version/' + chrome.runtime.getManifest().version;
     } catch(error) {
-      url = 'https://www.iflab.run/api/check/version/0.9.0';
+      url = 'https://www.iflab.run/api/check/version/0.9.1';
     }
     setTimeout(() => {
       axios.get(url)
